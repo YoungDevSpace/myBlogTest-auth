f903d11ce52c32f4e365dad71cba8c8c03cf3f55
YoungDevSpace <iamsoyoungdev@gmail.com>
2023-06-29 00:40:54 +0900

    init commit

---
+++ src/main/java/com/springmaster/myblogtest/MyBlogTestApplication.java
@@ -0,0 +1,13 @@
+package com.springmaster.myblogtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmaster/myblogtest/MyBlogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmaster.myblogtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
